61266f363548fcf156c7ef4396c07f89b9b60afd
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2022-06-14 02:42:28 -0300

    Adds more constants for external APIs.

--- pkg/configs/external.go
+++ pkg/configs/external.go
@@ -1,5 +1,9 @@
 package configs
 
 const (
+	WorkersCount = 5
 	MaxMockedResponseTimeInMillis = 900
+	AverageCoalesceStrategy = "average"
+	SumCoalesceStrategy = "sum"
+	CoalesceStrategyToUse = AverageCoalesceStrategy
 )
