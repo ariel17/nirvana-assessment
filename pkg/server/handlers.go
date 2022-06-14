59b0e6f23cfea2b6aab3872bed9f11e77da768e2
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2022-06-14 02:45:37 -0300

    Adds coalesce handler to implement what is required.

--- pkg/server/handlers.go
+++ pkg/server/handlers.go
@@ -2,23 +2,38 @@ package server
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 
 	"github.com/ariel17/nirvana-assessment/pkg/services"
 )
 
-// StatusHandler TODO
+// CoalesceHandler TODO
 // @Summary Shows the status of the application.
 // @Description TODO
 // @Accept json
 // @Produce json
-// @Router /status [get]
-func StatusHandler(c *gin.Context) {
-	status, err := services.GetStatus()
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, status)
+// @Router / [get]
+func CoalesceHandler(c *gin.Context) {
+	memberIDString := c.Query("member_id")
+	if memberIDString == "" {
+		c.JSON(http.StatusBadRequest, map[string]string{
+			"error": "member_id cannot be empty",
+		})
 		return
 	}
-	c.JSON(http.StatusOK, status)
+	memberID, err := strconv.Atoi(memberIDString)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, map[string]string{
+			"error": "member_id must be a number",
+		})
+		return
+	}
+	response, err := services.CoalesceAPIResponses(memberID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, map[string]error{"error": err})
+		return
+	}
+	c.JSON(http.StatusOK, response)
 }
--- pkg/server/handlers_test.go
+++ pkg/server/handlers_test.go
@@ -1,21 +1,44 @@
 package server
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
+
+	"github.com/ariel17/nirvana-assessment/pkg/services"
 )
 
-func TestStatusHandler(t *testing.T) {
-	r := gin.Default()
-	r.GET(statusPath, StatusHandler)
+func TestCoalesceHandler(t *testing.T) {
+	testCases := []struct{
+		name string
+		url string
+		statusCode int
+		response *services.Response
+	}{
+		{"correct member_id", "/?member_id=1", http.StatusOK, &services.Response{1066, 11000, 5666}},
+		{"empty member_id", "/", http.StatusBadRequest, nil},
+		{"non-numeric member_id", "/?member_id=x", http.StatusBadRequest, nil},
+	}
 
-	req, _ := http.NewRequest(http.MethodGet, statusPath, nil)
-	rr := httptest.NewRecorder()
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			r := gin.Default()
+			r.GET("/", CoalesceHandler)
 
-	r.ServeHTTP(rr, req)
-	assert.Equal(t, http.StatusOK, rr.Code)
+			req, _ := http.NewRequest(http.MethodGet, tc.url, nil)
+			rr := httptest.NewRecorder()
+
+			r.ServeHTTP(rr, req)
+			assert.Equal(t, tc.statusCode, rr.Code)
+
+			if tc.statusCode == http.StatusOK {
+				b, _ := json.Marshal(tc.response)
+				assert.Equal(t, string(b), rr.Body.String())
+			}
+		})
+	}
 }
